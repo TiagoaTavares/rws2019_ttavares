481163400a67fae7d4b9609a6809efdc5bf45106
TiagoaTavares <tiagoa.tavares@hotmail.com>
2019-02-21 10:56:31 +0000

    update

--- player_ttavares/src/player_ttavares.cpp
+++ player_ttavares/src/player_ttavares.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <ros/ros.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    ros::init(argc, argv, "player_ttavres");
+
+    // std::cout << "Hello World" << std::endl;
+
+    ros::Rate r(1); // p fazermos sleep a 1hz
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << std::endl;
+        r.sleep();
+    }
     return 1;
 }
