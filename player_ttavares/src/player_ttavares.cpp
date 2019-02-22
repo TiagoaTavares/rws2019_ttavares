358b369ded36165d288375241f9e84c65b76a97a
TiagoaTavares <tiagoa.tavares@hotmail.com>
2019-02-22 14:44:42 +0000

    getdisnatnce_update

--- player_ttavares/src/player_ttavares.cpp
+++ player_ttavares/src/player_ttavares.cpp
@@ -120,7 +120,7 @@ public:
     team_blue = (boost::shared_ptr<Team>)new Team("blue");
 
     ros::NodeHandle n;
-    vis_pub = (boost::shared_ptr<ros::Publisher>) new ros::Publisher ;
+    vis_pub = (boost::shared_ptr<ros::Publisher>)new ros::Publisher;
     (*vis_pub) = n.advertise<visualization_msgs::Marker>("/player_names", 0);
 
     if (team_red->playerBelongsToTeam(player_name)) // se o
@@ -154,14 +154,30 @@ public:
     printInfo();
   }
 
-
-
   void printInfo() {
     ROS_INFO_STREAM("My name is " << player_name << " and my team is "
                                   << team_mine->team_name << endl);
   }
 
+  
 
+  std::tuple<float, float> getDistanceAndAngleToPlayer(string other_player) 
+  {
+      tf::StampedTransform T0;
+    try {
+      listener.lookupTransform(player_name, other_player, ros::Time(0), T0);
+    } catch (tf::TransformException ex) {
+      ROS_ERROR("%s", ex.what());
+      ros::Duration(0.1).sleep();
+      return {1000,1000};
+    }
+
+    float d=sqrt(T0.getOrigin().x() * T0.getOrigin().x() + T0.getOrigin().y()*T0.getOrigin().y());
+    float a= atan2(T0.getOrigin().y(), T0.getOrigin().x());
+
+    return {d,a};
+
+  }
 
   void makeAPlayCallback(rws2019_msgs::MakeAPlayConstPtr msg) {
     ROS_INFO("received a new msg");
@@ -179,9 +195,36 @@ public:
     }
 
     // STEP 2: difine how i want to move
-    float dx = 0.5;
+    vector<float> distance_to_preys;
+    vector<float> angle_to_preys;
 
-    float a = M_PI / 31;
+    //For each prey find the closest. Tenh follow her:
+    for(size_t i =0; i< team_preys->player_names.size(); i++)
+    {
+        ROS_WARN_STREAM("team_preys =" << team_preys->player_names[i] << endl);
+
+        std::tuple<float, float> t= getDistanceAndAngleToPlayer(team_preys->player_names[i]);
+        distance_to_preys.push_back( std::get<0>(t));
+        angle_to_preys.push_back(std::get<1>(t));
+    }
+
+    int idx_closest_prey=0;
+    float distance_closest_prey = 1000;
+
+    for (size_t i=0; i<distance_to_preys.size(); i++)
+    {
+        if (distance_to_preys[i]<distance_closest_prey)
+        {
+            idx_closest_prey=i;
+            distance_closest_prey=distance_to_preys[i];
+        }
+    }
+
+
+
+    float dx = 10;
+
+    float a = angle_to_preys[idx_closest_prey];
     // STEP2.5: check values
     float dx_max = msg->turtle;
     dx > dx_max ? dx = dx_max : dx = dx;
@@ -201,8 +244,6 @@ public:
     br.sendTransform(
         tf::StampedTransform(Tglobal, ros::Time::now(), "world", player_name));
 
-
-
     visualization_msgs::Marker marker;
     marker.header.frame_id = player_name;
     marker.header.stamp = ros::Time();
@@ -215,11 +256,11 @@ public:
     marker.color.r = 0.0;
     marker.color.g = 1.0;
     marker.color.b = 0.0;
-    marker.text=player_name;
+    marker.text = player_name;
     // only if using a MESH_RESOURCE marker type:
-    //marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
+    // marker.mesh_resource =
+    // "package://pr2_description/meshes/base_v0/base.dae";
     vis_pub->publish(marker);
-
   }
 
 private:
